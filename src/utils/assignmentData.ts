9c0398cd4a5444dc8a08e7c2519ce256d06d1fe9
Abin <abin>
2024-07-12 13:22:16 +0530

    data changed

--- src/utils/assignmentData.ts
+++ src/utils/assignmentData.ts
@@ -11,36 +11,23 @@ export const assignments: assignmentDataTypes[]= [
     },
     {
         id: 1,
-        assignment: 'logo design',
+        assignment: 'UIUX Design - Ecommerce Mobile app',
         date: '10',
         month: 'July',
         time: '10:00 AM',
     },
     {
         id: 1,
-        assignment: 'logo design',
+        assignment: 'User Persona and User Journey',
         date: '10',
         month: 'July',
         time: '10:00 AM',
     },
     {
         id: 1,
-        assignment: 'logo design',
+        assignment: 'Typefaces',
         date: '10',
         month: 'July',
         time: '10:00 AM',
-    },  {
-        id: 1,
-        assignment: 'logo design',
-        date: '10',
-        month: 'July',
-        time: '10:00 AM',
-    },
-    {
-        id: 1,
-        assignment: 'logo design',
-        date: '10',
-        month: 'July',
-        time: '10:00 AM',
-    }, 
+    }
 ]
